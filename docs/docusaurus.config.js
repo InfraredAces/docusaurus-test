07e61db63f8234e48be7d64d7c73d6e278a4d6d6
InfraredAces <44279698+InfraredAces@users.noreply.github.com>
2023-10-05 23:01:53 +0000

    Recreate docusaurus files

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://infraredaces.github.io/docusaurus-test/',
+  url: 'https://infraredaces.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-test/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -69,7 +69,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
