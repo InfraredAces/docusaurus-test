a08dbe16dd6027cf0168dc011f40be734a58bae6
Kevin Nguyen <nguyenkevin625@gmail.com>
2023-10-15 15:14:40 -0700

    test local storage null

--- docs/src/components/LabelSelector.tsx
+++ docs/src/components/LabelSelector.tsx
@@ -4,7 +4,7 @@ import styles from '@site/src/components/labelselector.module.css';
 
 const SELECT_VALUE_KEY = "SelectedInputLabel";
 typeof window !== 'undefined' && window;
-if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined' && localStorage.getItem(SELECT_VALUE_KEY) == null) {
     localStorage.setItem(SELECT_VALUE_KEY, JSON.stringify({ value: "GP2040", label: "GP2040"}));
 }
 
